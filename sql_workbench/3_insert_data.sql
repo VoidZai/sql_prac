49b98c97a27aaf68f3ba88f48058f5ca6a597af8
Shaikhzai <shaikhston1123@gmail.com>
2025-09-28 18:29:45 +0530

    inserting data

---
+++ sql_workbench/3_insert_data.sql
@@ -0,0 +1,9 @@
+-- insert into users values
+-- (1, 'Zaid', 'zai@gmail.com', 'Female', '2000-05-23', default);
+
+insert into users (name, email, gender, date_of_birth) values
+('bobb', 'bobv@example.com', 'male', '2002-2-3'),
+('charlie', 'charlie@gmail.cpm', 'female', '2000-1-22'),
+('mia', 'khaliifa@example.com', 'female', '1999-12-12');
+
+select * from users; 
