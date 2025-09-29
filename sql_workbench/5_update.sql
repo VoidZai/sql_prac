528cc2664f772e8942584aaaa36e3655fce8b68f
Shaikhzai <shaikhston1123@gmail.com>
2025-09-29 10:42:35 +0530

    update

---
+++ sql_workbench/5_update.sql
@@ -0,0 +1,10 @@
+use startersql;
+-- update users set salary= 45400, email='balh@arav.com' where id=1;
+-- update users set salary=70000 where id=5;
+-- update users set name='Aisha Khan' where email='aisha@example.com';
+-- UPDATE users
+-- SET salary = salary + 10000
+-- WHERE salary < 60000
+--   AND id > 0;       -- forces use of key column
+
+select * from users;
